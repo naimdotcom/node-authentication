bd2a98b43bbed8a3e0ced3be627d4b29c214cf2f
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-04-21 21:45:24 +0600

    feat: add login functionality with validation and JWT token generation

--- src/Controller/auth.controller.js
+++ src/Controller/auth.controller.js
@@ -2,7 +2,7 @@ const { ApiResponse, ApiError } = require("../utils/ApiResponse");
 const User = require("../Model/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { hashPassword } = require("../Helper/bcrypt");
+const { hashPassword, comparePassword } = require("../Helper/bcrypt");
 const { generateToken } = require("../Helper/Jwt");
 
 const CreateUser = async (req, res) => {
@@ -46,6 +46,46 @@ const CreateUser = async (req, res) => {
       });
   } catch (error) {
     console.log("error While creating user: ", error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json(new ApiError(400, "All fields are required"));
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json(new ApiError(400, "User not found"));
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json(new ApiError(400, "Invalid credentials"));
+    }
+
+    const token = generateToken(
+      { id: user._id },
+      process.env.JWT_SECRET_EXPIRY_OTP
+    );
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, "User login successfully", {
+          token: `Bearer ${token}`,
+        })
+      )
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+      });
+  } catch (error) {
+    console.log("error while login user: ", error);
+    return res.status(500).json(new ApiError(500, "Internal server error"));
   }
 };
 
